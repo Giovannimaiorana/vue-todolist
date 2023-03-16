2ace67447b73a1b6a6afcf776f058219ae762dc1
Giovanni Maiorana <116109323+Giovannimaiorana@users.noreply.github.com>
2023-03-16 19:54:25 +0100

    milestone 1 finita

--- js/main.js
+++ js/main.js
@@ -3,7 +3,31 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      impegni:[
+        {
+          testo:'Fare spesa',
+          done: false
+        },
+
+        {
+          testo:'Consegnare Esercizio',
+          done: true
+        },
+
+        {
+          testo:'Scattare foto al tramonto',
+          done: false
+        }
+      ]
     }
+  },
+  methods:{
+eseguito(impegno){
+  if (impegno.done == false){
+    impegno.done == true;
+  }else{
+    impegno.done == false;
+  }
+}
   }
 }).mount('#app')
